6fd9fb23f66ef26f12009d0d1dd81beb707c1107
ravisolai12345 <inforavikrishnan@gmail.com>
2026-09-16 12:22:58 +0530

    Update firewall_services.sh to manage services

--- firewall_services.sh
+++ firewall_services.sh
@@ -9,34 +9,40 @@
 
 
 # Check current firewall services
+sudo firewall-cmd --list-services
 
 
 
 
 # Allow HTTP service
+sudo firewall-cmd --add-service=http
+
 
 
 
 # Allow HTTPS service
+sudo firewall-cmd --add-service=https
 
 
 
 
 # Display enabled services
+sudo firewall-cmd --list-services
 
 
 
 
 # Remove HTTP service
+sudo firewall-cmd --remove-service=http
 
 
 
 
 # Allow SSH permanently
+sudo firewall-cmd --permanent --add-service=ssh
 
 
 
 
 # Reload firewall configuration
-
-
+sudo firewall-cmd --reload
